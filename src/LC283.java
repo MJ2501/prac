35b425ee249e43ba4dd9041f045b62e400c04580
Mohit <jainmohit.2may@hotmail.com>
2026-03-11 08:03:46 +0530

    283. Move Zeroes

---
+++ src/LC283.java
@@ -0,0 +1,24 @@
+public class LC283 {
+    public void moveZeroes(int[] nums) {
+        int i = -1;
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] == 0) {
+                i = k;
+                break;
+            }
+        }
+        if (i == -1)
+            return;
+        int j = i + 1;
+        while (j <= nums.length - 1) {
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+            j++;
+        }
+        System.out.println(nums.toString());
+    }
+}
