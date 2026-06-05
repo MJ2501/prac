87998cc4b0300fd950c968f25b736c97b385e0fa
Mohit <jainmohit.2may@hotmail.com>
2026-06-06 00:17:40 +0530

    removed extra check for the solution

--- src/LC27.java
+++ src/LC27.java
@@ -29,10 +29,6 @@ public class LC27 {
 
     public int removeEle(int[] nums, int val){
         int count = 0;
-        if(nums.length == 1){
-            if(nums[0] == val) return 0;
-            else return 1;
-        }
         for(int i = 0 ;i < nums.length; i++){
             if(nums[i] != val){
                 nums[count] = nums[i];
